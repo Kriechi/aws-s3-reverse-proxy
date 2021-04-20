186e205807a6e0690550eb6f35e502690043bab8
Tim <truecarry@gmail.com>
2021-04-20 19:18:37 +0300

    rename metrics to have same prefix

--- metrics.go
+++ metrics.go
@@ -10,26 +10,26 @@ import (
 func wrapPrometheusMetrics(handler http.Handler) http.Handler {
 	counter := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "api_requests_total",
+			Name: "s3proxy_api_requests_total",
 			Help: "A counter for requests to the wrapped handler.",
 		},
 		[]string{"code", "method"},
 	)
 	duration := prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "request_duration_seconds",
+			Name:    "s3proxy_request_duration_seconds",
 			Help:    "A histogram of latencies for requests.",
 			Buckets: []float64{.25, .5, 0.75, 1, 2.5, 5, 10},
 		},
 		[]string{"handler", "method"},
 	)
 	inFlight := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "in_flight_requests",
+		Name: "s3proxy_in_flight_requests",
 		Help: "A gauge of requests currently being served by the wrapped handler.",
 	})
 	requestSize := prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "request_size_bytes",
+			Name:    "s3proxy_request_size_bytes",
 			Help:    "A histogram of request sizes.",
 			Buckets: []float64{200, 500, 900, 1500, 4100, 8200, 16400, 32800},
 		},
@@ -37,7 +37,7 @@ func wrapPrometheusMetrics(handler http.Handler) http.Handler {
 	)
 	responseSize := prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "response_size_bytes",
+			Name:    "s3proxy_response_size_bytes",
 			Help:    "A histogram of response sizes for requests.",
 			Buckets: []float64{200, 500, 900, 1500, 4100, 8200, 16400, 32800},
 		},
@@ -45,7 +45,7 @@ func wrapPrometheusMetrics(handler http.Handler) http.Handler {
 	)
 	timeToWriteHeader := prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "time_to_write_header",
+			Name:    "s3proxy_time_to_write_header",
 			Help:    "A histogram of time to write heaer.",
 			Buckets: []float64{0.25, 0.5, 0.75, 1, 2.5, 5, 10},
 		},
